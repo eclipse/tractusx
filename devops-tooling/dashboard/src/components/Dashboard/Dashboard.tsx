4d39ab8bca588d47705a1f9f4eecc1affac6e540
JaneMcBrain <contact@janemcbrain.com>
2021-11-23 17:20:58 +0100

    https://jira.catena-x.net/browse/A1ODT-29 move functionality to Dashboard

--- devops-tooling/dashboard/src/components/Dashboard/Dashboard.tsx
+++ devops-tooling/dashboard/src/components/Dashboard/Dashboard.tsx
@@ -13,46 +13,114 @@ import React, { RefObject } from 'react'
 import './Dashboard.scss'
 import * as d3 from 'd3'
 import data from './data.json';
-import { NetworkGraph } from '../NetworkGraph/NetworkGraph';
 
 export default class Dashboard extends React.PureComponent<IDashboardProps, IDashboardState> {
-  ref: RefObject<HTMLDivElement>
+  ref: RefObject<HTMLDivElement>;
+  simulation: any;
+  svg: any;
+  nodes: any;
+  links: any;
+  labels: any;
+  hierarchy: any;
 
   constructor(props: IDashboardProps) {
     super(props)
     this.state = {
       width: 640,
       height: 480,
-      nodes: [],
-      links: []
+      nodes: data.nodes.map((d: any) => Object.assign({}, d)),
+      links: data.links
     }
     this.ref = React.createRef()
   }
 
   componentDidMount() {
-    this.setState({nodes: [{x: 50, y: 60}, {x: 100, y: 200}]})
-    d3.select(this.ref.current).append('p').text('Hello World')
-    d3.select(this.ref.current).append('svg')
-      .attr('width', this.state.width)
-      .attr('height',  this.state.height);
-    d3.forceSimulation(this.state.nodes)
-    .force("link", d3.forceLink(this.state.links))
-    .force("charge", d3.forceManyBody().strength(-150))
-    .force("x", d3.forceX())
-    .force("y", d3.forceY());
+    this.createForceLayout();
+    this.createHierarchyLayout();
   }
 
-  nodeHoverTooltip() {
-    /* return React.useCallback((node) => {
-      return `<div>${node.name}</div>`;
-    }, []); */
-    console.log('hello');
-}
+  createForceLayout(){
+    this.svg = d3.select(this.ref.current).append('svg')
+      .attr("class", "graph")
+      .attr('viewBox', `${-this.state.width/2} ${-this.state.height/2} ${this.state.width} ${this.state.height}`)
+      .attr('width', this.state.width)
+      .attr('height',  this.state.height);
+
+    this.simulation = d3.forceSimulation(this.state.nodes)
+      .force("link", d3.forceLink(this.state.links))
+      .force("charge", d3.forceManyBody().strength(-2000))         // This adds repulsion between nodes. Play with the -400 for the repulsion strength 
+      .force("x", d3.forceX())
+      .force("y", d3.forceY());
+
+    this.nodes = this.svg
+      .append("g")
+      .attr("stroke", "#fff")
+      .attr("stroke-width", 2)
+      .selectAll("circle")
+      .data(this.state.nodes)
+      .join("circle")
+      .attr("r", 30)
+      .attr("fill", '#69b3a2')
+
+    this.links = this.svg
+      .append("g")
+      .attr("stroke", "#999")
+      .attr("stroke-opacity", 0.6)
+      .selectAll("line")
+      .data(this.state.links)
+      .join("line")
+      .attr("stroke-width", '2');
+
+    this.labels = this.svg.append("g")
+      .attr("class", "labels")
+      .selectAll("text")
+      .data(this.state.nodes)
+      .enter()
+      .append("text")
+      .attr('text-anchor', 'middle')
+      .attr('dominant-baseline', 'central')
+      .attr("class", (d: any) => d.gender)
+      .text((d: any) => d.name)
+    this.simulation.on("tick", () => {
+      this.positionForceElements();
+    });
+  }
+
+  positionForceElements() {
+    //update link positions
+    this.links
+      .attr("x1", (d: any) => d.source.x)
+      .attr("y1", (d: any) => d.source.y)
+      .attr("x2", (d: any) => d.target.x)
+      .attr("y2", (d: any) => d.target.y);
+
+  // update node positions
+    this.nodes
+      .attr("cx", (d: any) => d.x)
+      .attr("cy", (d: any) => d.y);
+
+    this.labels
+      .attr("x", (d: any) => d.x)
+      .attr("y", (d: any) => d.y);
+  }
+
+  createHierarchyLayout(){
+    this.svg = d3.select(this.ref.current).append('svg')
+      .attr("class", "hierarchy")
+      .attr('viewBox', `${-this.state.width/2} ${-this.state.height/2} ${this.state.width} ${this.state.height}`)
+      .attr('width', this.state.width)
+      .attr('height',  this.state.height);
+    
+    this.hierarchy = d3.forceSimulation(this.state.nodes)
+      .force("link", d3.forceLink(this.state.links))
+      .force("charge", d3.forceManyBody().strength(-2000))         // This adds repulsion between nodes. Play with the -400 for the repulsion strength 
+      .force("x", d3.forceX())
+      .force("y", d3.forceY());
+  }
 
   render() {
     return (
-      <div className="Dashboard" ref={this.ref}>
-        <NetworkGraph linksData={data.links} nodesData={data.nodes} nodeHoverTooltip={this.nodeHoverTooltip} />
+      <div className="dashboard" ref={this.ref}>
       </div>
     )
   }
@@ -66,6 +134,7 @@ interface IDashboardProps {
 interface IDashboardState {
   width: number,
   height: number,
-  nodes: {x: number, y: number}[], //needs to be its own type
+  nodes: any, //needs to be its own type
   links: { source: number, target: number }[]  //needs to be its own type
 }
+
